2d6d99fde621f2112d8954306edad240e771183a
yousufelmasri <yousufronaldo17@gmail.com>
2025-03-02 16:18:33 -0500

    Update Rule6-MET12-J.java

--- Rule6-MET12-J.java
+++ Rule6-MET12-J.java
@@ -1,4 +1,4 @@
-class MyFrame extends JFrame {
-  private byte[] buffer = new byte[16 * 1024 * 1024];
-  // Persists for at least two GC cycles
+class MyFrame {
+  private JFrame frame;
+  private byte[] buffer = new byte[16 * 1024 * 1024]; // Now decoupled
 }

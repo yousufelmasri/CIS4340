f9dca4dce195b4e03e0c6e849dd4d86bf5963ecc
yousufelmasri <yousufronaldo17@gmail.com>
2025-03-02 15:15:14 -0500

    Updated Cis4340.java with name

---
+++ Cis4340.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4340.java
+ *  Execution:    java Cis4340
+ *
+ *  Prints 
+ *    "Hello, World - from the class of cis4340 - Spring 2024 - UCF (Yousuf_Elmasri)".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4340
+ *  Hello, World - from the class of cis4340 - Spring 2024 - UCF (your name here)
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4340 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF (Yousuf_Elmasri)");
+    }
+
+}

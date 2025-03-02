7c58a9962202c9a1564d14bb8eae13000ef906e5
yousufelmasri <yousufronaldo17@gmail.com>
2025-03-02 15:40:23 -0500

    Update R04 STR03 J.java

--- R04 STR03 J.java
+++ R04 STR03 J.java
@@ -1,5 +1,5 @@
 BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+String s = x.toString();  // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray); 
+x = new BigInteger(ns);

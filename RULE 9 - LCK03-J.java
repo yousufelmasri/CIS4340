e03c36d7c4411d4e58bea720c4f72552d1456874
yousufelmasri <yousufronaldo17@gmail.com>
2025-03-02 16:18:10 -0500

    Update RULE 9 - LCK03-J.java

--- RULE 9 - LCK03-J.java
+++ RULE 9 - LCK03-J.java
@@ -1,7 +1,10 @@
 private final Lock lock = new ReentrantLock();
  
 public void doSomething() {
-  synchronized(lock) {
+  lock.lock();
+  try {
     // ...
+  } finally {
+    lock.unlock();
   }
 }

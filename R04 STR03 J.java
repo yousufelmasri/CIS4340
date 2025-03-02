b8aaf703586227871c078d0a333038bd5304b52d
yousufelmasri <yousufronaldo17@gmail.com>
2025-03-02 15:46:43 -0500

    Delete R04 STR03 J.java

--- R04 STR03 J.java
+++
@@ -1,5 +0,0 @@
-BigInteger x = new BigInteger("530500452766");
-String s = x.toString();  // Valid character data
-byte[] byteArray = s.getBytes();
-String ns = new String(byteArray); 
-x = new BigInteger(ns);
